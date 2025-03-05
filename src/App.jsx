6f573804b6ed099ad942c71513080a96fab328d4
bryanmayodi <bryanmayodi@gmail.com>
2025-03-05 10:17:00 +0300

    switched to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginForm from "./LoginForm";
 import PasswordChange from "./PasswordChange";
